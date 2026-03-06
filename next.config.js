38d17006e025f95fcec7009a4cae205253fc73f0
yonmardianto <yongithub@gmail.com>
2026-03-06 20:05:17 +0700

    fix csp blocked

--- next.config.js
+++ next.config.js
@@ -4,6 +4,19 @@ const nextConfig = {
   images: {
     domains: ['images.unsplash.com'],
   },
+  headers: async () => {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "script-src 'self' https://www.google.com https://www.gstatic.com 'unsafe-inline' 'unsafe-eval'; frame-src 'self' https://www.google.com https://recaptcha.google.com; connect-src 'self' https://www.google.com https://www.gstatic.com https://recaptcha.google.com;",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
